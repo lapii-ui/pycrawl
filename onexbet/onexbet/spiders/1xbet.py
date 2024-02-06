45a7d9191248bae01b1c7dbc16778aeb44e327cd
Maz Lapii <malapykin@gmail.com>
2024-02-07 01:16:01 +0700

    Init getting title of 1xbet

---
+++ onexbet/onexbet/spiders/1xbet.py
@@ -0,0 +1,23 @@
+import scrapy
+
+class Bet365(scrapy.Spider):
+    name = "1xbet"
+    start_urls = [
+        "https://1xbet.com/cn",
+        "https://1xbet.com/tw",
+        "https://1xbet.com/my",
+        "https://1xbet.com/en",
+        "https://1xbet.com/ko",
+        "https://1xbet.com/ja",
+        "https://1xbet.com/id",
+    ]
+
+    def parse(self, response):
+        title = response.css('#page_title span::text')[0].extract()
+        sport_types = response.xpath("//div[contains(@class, 'sports-slider__wrapper')]//div[contains(@class, 'b-filters__slide')]//a[contains(@class, 'b-filters__sport')]//div[contains(@class, 'b-filters__sport-name')]/text()").extract()
+        yield { 'group': title, 'sport_types': sport_types }
+        # elements = response.xpath("//div[contains(@class, 'sports-slider__wrapper')]//div[contains(@class, 'b-filters__slide')]//a[contains(@class, 'b-filters__sport')").extract()
+        # print('parents :: ', elements)
+        # # for element in elements:
+        #     url = element.xpath("//a[contains(@class, 'b-filters__sport')").xpath('@href').extract()
+        #     print('LOGG :: ', url)
